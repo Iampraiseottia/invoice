661a3fee4ef8fa7d16463290a6999654af24dbbb
pknowledge <yogesh.petel88@gmail.com>
2023-10-31 21:56:08 -1200

    amt and tax automatic calculation done using js

--- invoice.js
+++ invoice.js
@@ -1,3 +1,5 @@
+// DISPLAY & STORE TABLE VALUES, EDIT AND DELETE
+
 var selectedRow = null;
 
 
@@ -10,7 +12,6 @@ function onSubmit(){
 
 
     refreshData();
-
     
 }
 
@@ -23,7 +24,7 @@ function readData(){
 }
 
 function insertData(data){
-    var table = document.getElementById('invoiceTable').getElementsByTagName('tbody')[0];
+    var table = document.getElementById('invoiceTable').getElementsByTagName('tbody')[0], sum = 0;
     var newRow = table.insertRow(table.length); 
     cell1 = newRow.insertCell(0);
     cell1.innerHTML = data.description;
@@ -31,12 +32,20 @@ function insertData(data){
     cell2.innerHTML = data.amount;
     cell3 = newRow.insertCell(2);
     cell3.innerHTML = data.tax;
+    
     cell4 = newRow.insertCell(3);
     cell4.innerHTML = `<a href="" onClick="onEdit(this)">Edit</a>
                         <a href="" onClick="onDelete(this)">Delete</a>`;
 
+    
+                        // cell5 = newRow.insertCell(4);
+                        // cell5.innerHTML = `<a onchange="Calc(this)"></a>`;
+
+    
                         
 }
+
+//Thes is to automatically refresh the browser input field
 
 function refreshData(){
     document.getElementById('Descriptipon').value = '';
@@ -46,6 +55,8 @@ function refreshData(){
 
 }
 
+//edit each content
+
 function onEdit(td){
     event.preventDefault();
     selectedRow = td.parentElement.parentElement;
@@ -54,12 +65,16 @@ function onEdit(td){
     document.getElementById('Tax').value = selectedRow.cells[2].innerHTML;      
 }
 
+//update data in cell
+
 function updateData(inputData){
     selectedRow.cells[0].innerHTML = inputData.description;
     selectedRow.cells[1].innerHTML = inputData.amount;
     selectedRow.cells[2].innerHTML = inputData.tax;
 
 }
+
+//delete cell
 
 function onDelete(td){
     event.preventDefault();
@@ -68,3 +83,32 @@ function onDelete(td){
     refreshData();
 }
  
+//CALCULATION
+
+
+function Calc(all){
+    var tax = all.parentElement.parentElement.children[2].children[1].value;
+    var amt = all.parentElement.parentElement.children[1].children[1].value;
+    
+    var subtotal = amt * tax;
+    document.getElementById('total2').innerHTML = subtotal;
+
+    var table = document.getElementById('invoiceTable').getElementsByTagName('tbody')[0], sumAmt = 0, sumTax = 0;
+
+    for(var i = 0; i < table.rows.length; i++){
+        sumAmt = sumAmt + parseInt(table.rows[i].cells[1].innerHTML);
+
+    }
+    document.getElementById('sum').innerHTML = sumAmt;
+
+
+    for(var i = 0; i < table.rows.length; i++){
+        sumTax = sumTax + parseInt(table.rows[i].cells[1].innerHTML);
+
+    }
+    document.getElementById('boris').innerHTML = sumTax;
+
+    grandSum = sumAmt + sumTax;
+    document.getElementById('total').innerHTML = grandSum;
+
+}
