b6b692b4160442b43c79db974f1f0a9cf1e5b541
OTTIA PRAISE BETECK <ottiapraise50@gmail.com>
2024-01-10 08:29:59 -1200

    home main section completed

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+//Drop down
 var dropdown = document.querySelector('.dropdown');
 var dropdownContent = document.querySelector('.dropdown-content');
 
@@ -8,3 +9,8 @@ dropdown.addEventListener('mouseover', function () {
 dropdown.addEventListener('mouseout', function () {
     dropdownContent.style.display = 'none';
 });
+
+
+//Animate Image
+var image = document.getElementById("animate-image");
+image.classList.add("animate");
